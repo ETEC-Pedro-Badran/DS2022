76e4e0b146a811fcb37b3836b77cfe8181675f6b
evaldo <evaldo@parser.com.br>
2022-03-24 11:30:19 -0300

    Atribuição pelo método construtor.

---
+++ construtor/cadeira.class.php
@@ -0,0 +1,32 @@
+<?php
+class Cadeira {
+    public $patrimonio;
+    public $id;
+    public $material;
+    public $modelo;
+    public $preco;
+    public $dataAquisicao;
+
+
+ 
+
+    function toString(){
+       return "Cadeira $this->modelo - id: $this->id" ;
+    }
+
+
+    function __construct($patrimonio, $id, $material, $modelo, $preco, $dataAquisicao)
+    {
+        $this->patrimonio = $patrimonio;
+        $this->id = $id;
+        $this->material = $material;
+        $this->modelo = $modelo;
+        $this->preco = $preco;
+        $this->dataAquisicao = $dataAquisicao;
+    }
+
+}
+
+
+
+
